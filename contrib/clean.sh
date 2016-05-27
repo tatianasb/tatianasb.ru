74fab8ef76e9b11109958d1d979c0d6d9adf735c
Eduardo Cuducos <cuducos@gmail.com>
2016-05-26 21:05:54 -0300

    Use -delete instead of -exec rm

--- contrib/clean.sh
+++ contrib/clean.sh
@@ -1,4 +1,4 @@
 echo 'Removing Icon? files'
-find . -iname 'Icon*' -exec rm {} \;
+find . -iname 'Icon*' -delete
 echo 'Removing .DS_Store files'
-find . -iname '.DS_Store' -exec rm {} \;
+find . -iname '.DS_Store' -delete
