b56ac2e64384d7c4ddc81aa2378d6095ee1f4131
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-11-15 15:40:05 -0200

    Deploy to S3

--- contrib/clean.sh
+++
@@ -1,4 +0,0 @@
-echo 'Removing Icon? files'
-find . -iname 'Icon*' -delete
-echo 'Removing .DS_Store files'
-find . -iname '.DS_Store' -delete
--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,26 +1,18 @@
-if [ -n "$TATIANASBRU" ]; then
+echo '\n==> Cleaning system created files'
+find . -iname '.DS_Store' -delete
+find . -iname 'Icon*' -delete
 
-    echo '\n==> Instaling NodeJS and Bower dependencies'
-    npm install
+echo '\n==> Instaling NodeJS and Bower dependencies'
+npm install
 
-    echo '\n==> Generating JavaScript'
-    npm run js
+echo '\n==> Generating JavaScript'
+npm run js
 
-    echo '\n==> Copying Font-Awesome font files to assets/'
-    npm run fonts
+echo '\n==> Copying Font-Awesome font files to assets/'
+npm run fonts
 
-    echo '\n==> Cleaning system created files'
-    ./contrib/clean.sh
+echo '\n==> Generating the site at _site/'
+JEKYLL_ENV=production bundle exec jekyll build
 
-    echo '\n==> Generating the site at _site/'
-    JEKYLL_ENV=production bundle exec jekyll build
-
-    echo '\n==> Syncing with the remote sever'
-    rsync -crvz --delete _site/ $TATIANASBRU
-
-else
-
-    echo '\nPlease, set the environment variable `TATIANASBRU` with the server SSH info.'
-    echo '(e.g. `export TATIANASBRU=user:secret@server.org:/users/user/www/`)\n'
-
-fi
+echo '\n==> Syncing with the remote sever'
+aws s3 sync _site s3://tatianasb.ru --profile tatiana --delete
